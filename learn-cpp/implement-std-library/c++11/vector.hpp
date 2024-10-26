ec5ecd081a979043b542490484752d0b86d81302
liginity <liginity@outlook.com>
2024-10-26 06:35:41 +0000

    Format vector.hpp with clang-format

--- learn-cpp/implement-std-library/c++11/vector.hpp
+++ learn-cpp/implement-std-library/c++11/vector.hpp
@@ -5,10 +5,9 @@ namespace learn_cpp {
 
 namespace detail {
 
-  template<class T, class Allocator = allocator<T>>
-  class vector
-  {
-  public:
+template <class T, class Allocator = allocator<T>>
+class vector {
+   public:
     // types
     // clang-format off
     using value_type             = T;
@@ -24,90 +23,93 @@ namespace detail {
     using reverse_iterator       = std::reverse_iterator<iterator>;
     using const_reverse_iterator = std::reverse_iterator<const_iterator>;
     // clang-format on
- 
+
     // construct/copy/destroy:
     explicit vector(const Allocator& = Allocator());
     explicit vector(size_type n);
-    vector(size_type n, const T& value,const Allocator& = Allocator());
+    vector(size_type n, const T& value, const Allocator& = Allocator());
     template <class InputIterator>
-        vector(InputIterator first, InputIterator last,const Allocator& = Allocator());
-    vector(const vector<T,Allocator>& x);
+    vector(InputIterator first, InputIterator last,
+           const Allocator& = Allocator());
+    vector(const vector<T, Allocator>& x);
     vector(vector&&);
     vector(const vector&, const Allocator&);
     vector(vector&&, const Allocator&);
     vector(initializer_list<T>, const Allocator& = Allocator());
- 
+
     ~vector();
-    vector<T,Allocator>& operator=(const vector<T,Allocator>& x);
-    vector<T,Allocator>& operator=(vector<T,Allocator>&& x);
+    vector<T, Allocator>& operator=(const vector<T, Allocator>& x);
+    vector<T, Allocator>& operator=(vector<T, Allocator>&& x);
     vector& operator=(initializer_list<T>);
     template <class InputIterator>
-        void assign(InputIterator first, InputIterator last);
+    void assign(InputIterator first, InputIterator last);
     void assign(size_type n, const T& t);
     void assign(initializer_list<T>);
     allocator_type get_allocator() const noexcept;
- 
+
     // iterators:
-    iterator                begin() noexcept;
-    const_iterator          begin() const noexcept;
-    iterator                end() noexcept;
-    const_iterator          end() const noexcept;
- 
-    reverse_iterator        rbegin() noexcept;
-    const_reverse_iterator  rbegin() const noexcept;
-    reverse_iterator        rend() noexcept;
-    const_reverse_iterator  rend() const noexcept;
- 
-    const_iterator          cbegin() noexcept;
-    const_iterator          cend() noexcept;
-    const_reverse_iterator  crbegin() const noexcept;
-    const_reverse_iterator  crend() const noexcept;
- 
+    iterator begin() noexcept;
+    const_iterator begin() const noexcept;
+    iterator end() noexcept;
+    const_iterator end() const noexcept;
+
+    reverse_iterator rbegin() noexcept;
+    const_reverse_iterator rbegin() const noexcept;
+    reverse_iterator rend() noexcept;
+    const_reverse_iterator rend() const noexcept;
+
+    const_iterator cbegin() noexcept;
+    const_iterator cend() noexcept;
+    const_reverse_iterator crbegin() const noexcept;
+    const_reverse_iterator crend() const noexcept;
+
     // capacity:
     size_type size() const noexcept;
     size_type max_size() const noexcept;
-    void      resize(size_type sz);
-    void      resize(size_type sz, const T& c);
+    void resize(size_type sz);
+    void resize(size_type sz, const T& c);
     size_type capacity() const noexcept;
-    bool      empty() const noexcept;
-    void      reserve(size_type n);
-    void      shrink_to_fit();
- 
+    bool empty() const noexcept;
+    void reserve(size_type n);
+    void shrink_to_fit();
+
     // element access:
-    reference       operator[](size_type n);
+    reference operator[](size_type n);
     const_reference operator[](size_type n) const;
-    reference       at(size_type n);
+    reference at(size_type n);
     const_reference at(size_type n) const;
-    reference       front();
+    reference front();
     const_reference front() const;
-    reference       back();
+    reference back();
     const_reference back() const;
- 
-    //data access
-    T*       data() noexcept;
+
+    // data access
+    T* data() noexcept;
     const T* data() noexcept;
- 
+
     // modifiers:
-    template <class... Args> void emplace_back(Args&&... args);
+    template <class... Args>
+    void emplace_back(Args&&... args);
     void push_back(const T& x);
     void push_back(T&& x);
     void pop_back();
- 
-    template <class... Args> iterator emplace(const_iterator position, Args&&... args);
+
+    template <class... Args>
+    iterator emplace(const_iterator position, Args&&... args);
     iterator insert(const_iterator position, const T& x);
     iterator insert(const_iterator position, T&& x);
     iterator insert(const_iterator position, size_type n, const T& x);
     template <class InputIterator>
-        iterator insert (const_iterator position, InputIterator first, 
-                         InputIterator last);
+    iterator insert(const_iterator position, InputIterator first,
+                    InputIterator last);
     iterator insert(const_iterator position, initializer_list<T>);
- 
+
     iterator erase(const_iterator position);
     iterator erase(const_iterator first, const_iterator last);
-    void     swap(vector<T,Allocator>&);
-    void     clear() noexcept;
+    void swap(vector<T, Allocator>&);
+    void clear() noexcept;
 }
 
-}
-}
+}  // namespace detail
+}  // namespace learn_cpp
 #endif
