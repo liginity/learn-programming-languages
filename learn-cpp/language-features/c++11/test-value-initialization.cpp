a9ebd93bdc2e2d6de40d477aa4a6db91f310853e
liginity <liginity@outlook.com>
2025-03-19 21:43:08 +0800

    Show more clear cases

--- learn-cpp/language-features/c++11/test-value-initialization.cpp
+++ learn-cpp/language-features/c++11/test-value-initialization.cpp
@@ -17,7 +17,7 @@ void f1(int i);
 int main() {
     constexpr int N = 4;
     for (int i = 0; i < N; ++i) {
-        std::cout << "#" << i << "\n";
+        // std::cout << "#" << i << "\n";
         f1(i);
     }
 }
@@ -30,6 +30,8 @@ void f1(int i) {
               << ", mem2 = " << item1_obj1.mem2_ << "\n";
     std::cout << "Item2 (default init): mem1 = " << item2_obj1.mem1_
               << ", mem2 = " << item2_obj1.mem2_ << "\n";
+    std::cout << "&item1_obj1 = " << &item1_obj1
+              << ", &item2_obj1 = " << &item2_obj1 << "\n";
 
     item1_obj1.mem1_ = 10 + i;
     item1_obj1.mem2_ = 100 + i;
@@ -49,13 +51,22 @@ void f1(int i) {
     item1_obj2.mem2_ = 100 + i;
     item2_obj2.mem1_ = 10 + i;
     item2_obj2.mem2_ = 100 + i;
+
+    std::cout << "#" << i << "\n";
 }
 
 /* one result
 
-#0
-Item1 (default init): mem1 = 1906839864, mem2 = 32764
-Item2 (default init): mem1 = 0, mem2 = 32764
+Item1 (default init): mem1 = 0, mem2 = 0
+Item2 (default init): mem1 = 0, mem2 = 0
+&item1_obj1 = 0x7fff34968d98, &item2_obj1 = 0x7fff34968d90
 Item1 (value init)  : mem1 = 0, mem2 = 0
-Item2 (value init)  : mem1 = 0, mem2 = 32571
+Item2 (value init)  : mem1 = 0, mem2 = 0
+#0
+Item1 (default init): mem1 = 10, mem2 = 100
+Item2 (default init): mem1 = 0, mem2 = 100
+&item1_obj1 = 0x7fff34968d98, &item2_obj1 = 0x7fff34968d90
+Item1 (value init)  : mem1 = 0, mem2 = 0
+Item2 (value init)  : mem1 = 0, mem2 = 100
+#1
  */
