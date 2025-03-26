3870a0ed69fdbe51ccd7ad783ce750c842592c55
liginity <liginity@outlook.com>
2025-03-26 13:14:00 +0800

    Add intro-frame.py

---
+++ learn-python/tutorial-by-gaotian/not-basic-basics/frame/intro-frame.py
@@ -0,0 +1,48 @@
+import inspect
+import pprint
+# import sys
+# from typing import Type
+
+
+def f() -> None:
+    frame = inspect.currentframe()
+    # print(frame)
+    # pprint.pprint(frame)
+    # pprint.pprint(dir(frame))
+    for attr in dir(frame):
+        if not attr.startswith("f_"):
+            continue
+        # print(f".{attr} = {getattr(frame, attr)}")
+
+        print(f".{attr} = ", end="")
+        if isinstance(getattr(frame, attr), frame.__class__):
+            print(frame.__class__)
+            continue
+        if attr == "f_builtins":
+            print("<builtins dict...>")
+            continue
+        pprint.pprint(getattr(frame, attr), depth=1)
+
+
+f()
+"""
+The call stack frames are organized as a linked list.
+They are connected with `.f_back`.
+"""
+
+
+print("-" * 80)
+
+
+def f2() -> None:
+    # frame: type(sys._getframe()) = inspect.currentframe()
+    # frame: Type[sys._getframe()] = inspect.currentframe()
+    frame = inspect.currentframe()
+    print(f"{frame.f_back.f_code.co_name = }")  # type: ignore
+
+
+def g2():
+    f2()
+
+
+g2()
